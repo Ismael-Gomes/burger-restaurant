0b3477544b74161e6752c9e0de1556386704996d
ismael <ismael.silva.dev@gmail.com>
2024-09-21 14:22:45 -0300

    feat - Adiciona telas iniciais com modulos de acesso ao software

--- src/main.c
+++ src/main.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
 int main(void) {
-    
+
+//Pedido de Login
+
+    printf("=========================================\n");
+    printf("|               BEM VINDO!              |\n");
+    printf("=========================================\n");
+    printf("|---------------------------------------|\n");
+    printf("|                  LOGIN                |\n");
+    printf("#### 1 - Login                          |\n");
+    printf("#### 2 - Não Possuo Cadastro            |\n");
+    printf("----------------------------------------|\n");
+    printf("#### Selecione uma das opcoes: ");
+
+
+//Caso o login tenha permissão de admin
+
+    printf("\n=========================================\n");
+    printf("|           BURGUER RESTAURANT          |\n");
+    printf("=========================================\n");
+    printf("|---------------------------------------|\n");
+    printf("|    Bem-vindo a Burguer Restaurant!    |\n");
+    printf("----------------------------------------|\n");
+    printf("#### 1 - Gerenciamento de Produtos      |\n");
+    printf("#### 2 - Gerenciamento de Funcionários  |\n");
+    printf("#### 3 - Pedidos                        |\n");
+    printf("#### 4 - Relatório                      |\n");
+    printf("#### 5 - Sair                           |\n");
+    printf("----------------------------------------|\n");
+    printf("#### Selecione uma das opcoes: ");
+
+
+//Caso o login não tenha permissão de admin
+
+    printf("\n=========================================\n");
+    printf("|           BURGUER RESTAURANT          |\n");
+    printf("=========================================\n");
+    printf("|---------------------------------------|\n");
+    printf("|    Bem-vindo a Burguer Restaurant!    |\n");
+    printf("----------------------------------------|\n");
+    printf("#### 1 - Cardápio                       |\n");
+    printf("#### 2 - Realizar Compra                |\n");
+    printf("#### 3 - Status da Compra               |\n");
+    printf("#### 4 - Meu Perfil                     |\n");
+    printf("#### 5 - Sair                           |\n");
+    printf("----------------------------------------|\n");
+    printf("#### Selecione uma das opcoes: ");
 }
