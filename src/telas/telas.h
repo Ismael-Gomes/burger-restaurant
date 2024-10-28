603b6bcb7d745a035f0e1069a0b6d6e3ab28d30f
Kaique Vieira <123115955+kaiquevieirasoares@users.noreply.github.com>
2024-10-28 19:54:24 -0300

    modularizacao das telas

---
+++ src/telas/telas.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+void telaLogin(void);
+void menuAdmin(void);
+void telaGerenciaProduto(void);
+void telaGerenciaHamburguer(void);
+void telaGerenciaBebida(void);
+void telaGerenciaFuncionario(void);
+void telaGerenciaPedido(void);
+void telaRelatorio(void);
+void menuCliente(void);
+void telaMenuCardapio(void);
+void telaMenuCompra(void);
+void telaStatusCompra(void);
+void telaPerfilCliente(void);
+
+#endif
