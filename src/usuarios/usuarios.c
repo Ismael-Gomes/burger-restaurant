55388ef421224167637f504b6b7f03c9900b0c3a
Ismael Gomes <122225294+Ismael-Gomes@users.noreply.github.com>
2024-11-04 22:01:39 -0300

    feat - validacao de dados
    foi adicionado a logica para a validacao dos dados inseridos

--- src/usuarios/usuarios.c
+++ src/usuarios/usuarios.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "usuarios.h"
+#include "validacoes.h"
 
 void cadastrarUsuario(Usuario usuarios[], int *totalUsuarios) {
     if (*totalUsuarios >= 10) {
@@ -13,12 +14,15 @@ void cadastrarUsuario(Usuario usuarios[], int *totalUsuarios) {
 
     printf("Digite o nome do Usuário:\n");
     scanf(" %[^\n]%*c", novoUsuario.nome);
+    if (!validarNome(novoUsuario.nome)) return;
 
     printf("Digite o email do Usuário:\n");
     scanf("%s", novoUsuario.email);
+    if (!validarEmail(novoUsuario.email)) return;
 
     printf("Digite a senha do Usuário:\n");
     scanf("%s", novoUsuario.senha);
+    if (!validarSenha(novoUsuario.senha)) return;
 
     novoUsuario.admin = false;
     usuarios[*totalUsuarios] = novoUsuario;
