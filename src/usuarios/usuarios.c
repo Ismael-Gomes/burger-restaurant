96b19e370ea3a3eecf26ff643cd4a75d86703cc6
lelebiglove <isaac.brito.136@ufrn.edu.br>
2024-11-25 13:24:01 -0300

    data

--- src/usuarios/usuarios.c
+++ src/usuarios/usuarios.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "usuarios.h"
 #include "validacoes.h"
-#define ARQUIVO_USUARIOS "usuarios.dat"
+#define usuarios.dat"
 
 void salvarUsuariosEmArquivo(Usuario usuarios[], int totalUsuarios) {
     FILE *arquivo = fopen(ARQUIVO_USUARIOS, "wb");
