bbc8fb92d1531b6a02031511af427acfcb97d7f1
lelebiglove <isaac.brito.136@ufrn.edu.br>
2024-11-25 13:21:19 -0300

    usuários

--- src/usuarios/usuarios.c
+++ src/usuarios/usuarios.c
@@ -2,7 +2,36 @@
 #include <string.h>
 #include "usuarios.h"
 #include "validacoes.h"
+#define ARQUIVO_USUARIOS "usuarios.dat"
 
+void salvarUsuariosEmArquivo(Usuario usuarios[], int totalUsuarios) {
+    FILE *arquivo = fopen(ARQUIVO_USUARIOS, "wb");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para salvar os usuários.\n");
+        return;
+    }
+
+    fwrite(&totalUsuarios, sizeof(int), 1, arquivo); // Salva o total de usuários
+    fwrite(usuarios, sizeof(Usuario), totalUsuarios, arquivo); // Salva os dados dos usuários
+
+    fclose(arquivo);
+    printf("Usuários salvos com sucesso!\n");
+}
+
+int carregarUsuariosDeArquivo(Usuario usuarios[]) {
+    FILE *arquivo = fopen(ARQUIVO_USUARIOS, "rb");
+    if (arquivo == NULL) {
+        printf("Nenhum arquivo encontrado. Iniciando com dados padrão.\n");
+        return 0;  
+    }
+
+    int totalUsuarios;
+    fread(&totalUsuarios, sizeof(int), 1, arquivo); // Lê o total de usuários
+    fread(usuarios, sizeof(Usuario), totalUsuarios, arquivo); // Lê os dados dos usuários
+
+    fclose(arquivo);
+    return totalUsuarios;  // MOStra o total de usuários
+}
 void cadastrarUsuario(Usuario usuarios[], int *totalUsuarios) {
     if (*totalUsuarios >= 10) {
         printf("Erro: Não há mais espaço para novos usuários.\n");
