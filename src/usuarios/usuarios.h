d7c38106410d91eadd0dcac95a4bd8161d61daf1
lelebiglove <isaac.brito.136@ufrn.edu.br>
2024-11-25 13:33:03 -0300

    usários

--- src/usuarios/usuarios.h
+++ src/usuarios/usuarios.h
@@ -1,17 +1,23 @@
-#ifndef USUARIO_H
-#define USUARIO_H
+#ifndef USUARIOS_H
+#define USUARIOS_H
 
 #include <stdbool.h>
 
 typedef struct {
     int id;
     char nome[100];
-    char email[100];
+    char email[50];
     char senha[50];
     bool admin;
 } Usuario;
 
 int validarUsuario(Usuario usuarios[], int totalUsuarios, char email[], char senha[]);
+
+
 void cadastrarUsuario(Usuario usuarios[], int *totalUsuarios);
+
+
+void salvarUsuarios(Usuario usuarios[], int totalUsuarios);
+int carregarUsuarios(Usuario usuarios[]);
 
 #endif
