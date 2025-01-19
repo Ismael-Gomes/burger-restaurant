e61f1f6763fca47b583cbf99e05dce7c82879673
lelebiglove <isaac.brito.136@ufrn.edu.br>
2025-01-19 00:00:49 -0300

    relatorio

---
+++ src/relatorios/relatorio.h
@@ -0,0 +1,10 @@
+#ifndef RELATORIO_H
+#define RELATORIO_H
+
+void telaRelatorio();
+void gerarRelatorio(int opcao);
+void relatorioProdutos();
+void relatorioFuncionarios();
+void relatorioPedidos();
+
+#endif
