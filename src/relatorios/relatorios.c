f17b40fc61c5eb6d35a23ef8ba477fe76727760d
lelebiglove <isaac.brito.136@ufrn.edu.br>
2025-01-18 23:35:19 -0300

    relatori

---
+++ src/relatorios/relatorios.c
@@ -0,0 +1,15 @@
+#include "relatorio.h"
+#include <stdio.h>
+
+void telaRelatorio() {
+    printf("\\n\\n");
+    printf("///           = = = = = = = = = Relatórios = = = = = = = = =             ///\\n");
+    printf("///                                                                       ///\\n");
+    printf("///           1. Relatório de Produtos                                    ///\\n");
+    printf("///           2. Relatório de Funcionários                                ///\\n");
+    printf("///           3. Relatório de Pedidos                                     ///\\n");
+    printf("///           0. Voltar                                                   ///\\n");
+    printf("///                                                                       ///\\n");
+    printf("///           Escolha a opção desejada:                                   ///\\n");
+    printf("\\n\\n");
+}
