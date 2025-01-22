54c868437ebfd9e6dad324936ded49435b684f50
Caio Lucas <caiolucas0430@gmail.com>
2025-01-22 15:11:17 -0300

    add modulos

---
+++ src/produtos1/produto_arquivo.h
@@ -0,0 +1,7 @@
+#ifndef LER_PRODUTOS_H
+#define LER_PRODUTOS_H
+void ler_id_codigo(int *id_codigo);
+void ler_nome_produto(char *nome, size_t tamanho);
+void ler_quantidade(int *quantidade);
+void ler_valor_produto(char *valor, size_t tamanho);
+#endif
