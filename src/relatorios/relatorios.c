3f8de4199d4ced7793396fbd3e01eb2482dae46a
Caio Lucas <caiolucas0430@gmail.com>
2025-01-22 11:45:28 -0300

    correções

--- src/relatorios/relatorios.c
+++ src/relatorios/relatorios.c
@@ -1,18 +1,7 @@
 #include "relatorio.h"
 #include <stdio.h>
 
-void telaRelatorio() {
-    printf("\\n\\n");
-    printf("///           = = = = = = = = = Relatórios = = = = = = = = =             ///\\n");
-    printf("///                                                                       ///\\n");
-    printf("///           1. Relatório de Produtos                                    ///\\n");
-    printf("///           2. Relatório de Funcionários                                ///\\n");
-    printf("///           3. Relatório de Pedidos                                     ///\\n");
-    printf("///           0. Voltar                                                   ///\\n");
-    printf("///                                                                       ///\\n");
-    printf("///           Escolha a opção desejada:                                   ///\\n");
-    printf("\\n\\n");
-}
+
  void gerarRelatorio(int opcao) {
     switch (opcao) {
         case 1:
